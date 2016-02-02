a2fbc7c3473c9227973a3bf4964f183653aec27b
Bob Saveland <bob.saveland@teamaol.com>
2016-02-02 18:19:44 -0500

    changed producer to take hash of options

--- lib/jruby/kafka_producer.rb
+++ lib/jruby/kafka_producer.rb
@@ -2,46 +2,50 @@ require 'jruby/kafka'
 
 class KafkaProducer
 
-  def initialize(broker_url, key_serializer = :string, value_serializer = :string, schema_repo_url=nil)
-    config = Java::JavaUtil::Properties.new
-    config['bootstrap.servers'] = broker_url
+  def initialize(broker_url, key_serializer = :string, value_serializer = :string, opts={})
+    @config = Java::JavaUtil::Properties.new
+    @config['bootstrap.servers'] = broker_url
+    opts.each {|k,v| @config[k] = v}
+    set_serializers(key_serializer, value_serializer)
 
+    @producer = Java::OrgApacheKafkaClientsProducer::KafkaProducer.new @config
+  end
+
+  def set_serializers(key_serializer, value_serializer)
     case key_serializer
     when :string, "string"
-      config['key.serializer'] = "org.apache.kafka.common.serialization.StringSerializer"
+      @config['key.serializer'] = "org.apache.kafka.common.serialization.StringSerializer"
     when :byte_array, "byte_array"
-      config['key.serializer'] = "org.apache.kafka.common.serialization.ByteArraySerializer"
+      @config['key.serializer'] = "org.apache.kafka.common.serialization.ByteArraySerializer"
     when :avro, "avro"
       require 'jruby/avro_serializer'
-      config['key.serializer'] = "io.confluent.kafka.serializers.KafkaAvroSerializer"
+      @config['key.serializer'] = "io.confluent.kafka.serializers.KafkaAvroSerializer"
     else
       raise ArgumentError, "key_serializer must be :string, :byte_array, or :avro"
     end
 
     case value_serializer
     when :string, "string"
-      config['value.serializer'] = "org.apache.kafka.common.serialization.StringSerializer"
+      @config['value.serializer'] = "org.apache.kafka.common.serialization.StringSerializer"
     when :byte_array, "byte_array"
-      config['value.serializer'] = "org.apache.kafka.common.serialization.ByteArraySerializer"
+      @config['value.serializer'] = "org.apache.kafka.common.serialization.ByteArraySerializer"
     when :avro, "avro"
       require 'jruby/avro_serializer'
-      config['value.serializer'] = "io.confluent.kafka.serializers.KafkaAvroSerializer"
+      @config['value.serializer'] = "io.confluent.kafka.serializers.KafkaAvroSerializer"
     else
       raise ArgumentError, "value_serializer must be :string, :byte_array, or :avro"
     end
 
     if key_serializer.to_s == "avro" || value_serializer.to_s == "avro"
-      raise ArgumentError, "schema_repo_url required with avro serializer" unless schema_repo_url
-      config['schema.registry.url'] = schema_repo_url
+      raise ArgumentError, "schema_repo_url required with avro serializer" unless @config['schema.registry.url']
       if $DEBUG
         l = Java::OrgApacheLog4j::Logger.get_logger "io.confluent"
         l.set_level(Java::OrgApacheLog4j::Level::DEBUG)
         l.add_appender Java::OrgApacheLog4j::ConsoleAppender.new(Java::OrgApacheLog4j::SimpleLayout.new, Java::OrgApacheLog4j::ConsoleAppender::SYSTEM_OUT)
       end
     end
-
-    @producer = Java::OrgApacheKafkaClientsProducer::KafkaProducer.new config
   end
+  private :set_serializers
 
   def queue(topic, key, message)
     @producer.send Java::OrgApacheKafkaClientsProducer::ProducerRecord.new(topic, key, message)
